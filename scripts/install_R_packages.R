e6d10ef7c2226345c9cc685a4bf59c075d37841d
achevali <aaronc@broadinstitute.org>
2015-06-01 16:08:51 -0400

    Also install Argparser

--- scripts/install_R_packages.R
+++ scripts/install_R_packages.R
@@ -1,2 +1,7 @@
 source("http://bioconductor.org/biocLite.R")
 biocLite("DNAcopy")
+dependencies = c("argparser")
+if (length(setdiff(dependencies, rownames(installed.packages()))) > 0) {
+  install.packages(setdiff(dependencies, rownames(installed.packages())), repos="http://cran.cnr.Berkeley.edu")
+}
+q(save="no")
