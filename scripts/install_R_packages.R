d2a1ecd47f712fa87062cda529e9f19d2c74f7c5
achevali <aaronc@broadinstitute.org>
2015-07-15 11:50:34 -0400

    Comments from pull request

--- scripts/install_R_packages.R
+++ scripts/install_R_packages.R
@@ -1,11 +1,11 @@
 source("http://bioconductor.org/biocLite.R")
 biocLite("DNAcopy")
 argparserurl="http://cran.r-project.org/src/contrib/Archive/argparser/argparser_0.1.tar.gz"
-if (length(setdiff("argparser", rownames(installed.packages()))) > 0) {
+if ("argparser" %in% rownames(installed.packages)) {
   install.packages(argparserurl, repos=NULL, type="source")
 }
 dependencies = c("naturalsort")
-if (length(setdiff(dependencies, rownames(installed.packages()))) > 0) {
+if (!all(dependencies %in% rownames(installed.packages))) {
   install.packages(setdiff(dependencies, rownames(installed.packages())), repos="http://cran.cnr.Berkeley.edu")
 }
 q(save="no")
