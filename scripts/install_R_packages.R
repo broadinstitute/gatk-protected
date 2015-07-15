10e7a695c727551caa071ff96916c2961e59a2c7
achevali <aaronc@broadinstitute.org>
2015-07-15 11:59:54 -0400

    Add needed not operator

--- scripts/install_R_packages.R
+++ scripts/install_R_packages.R
@@ -1,7 +1,7 @@
 source("http://bioconductor.org/biocLite.R")
 biocLite("DNAcopy")
 argparserurl="http://cran.r-project.org/src/contrib/Archive/argparser/argparser_0.1.tar.gz"
-if ("argparser" %in% rownames(installed.packages)) {
+if (!("argparser" %in% rownames(installed.packages))) {
   install.packages(argparserurl, repos=NULL, type="source")
 }
 dependencies = c("naturalsort")
