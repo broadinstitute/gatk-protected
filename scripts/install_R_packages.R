b0acab15df1b68164509d0c9d6549121153ac879
achevali <aaronc@broadinstitute.org>
2015-07-20 16:24:25 -0400

    --args argument allows Rscript to recognize which arguments are which. Switched to optparse which is a more mature library.

--- scripts/install_R_packages.R
+++ scripts/install_R_packages.R
@@ -1,9 +1,10 @@
 source("http://bioconductor.org/biocLite.R")
 biocLite("DNAcopy")
-argparserurl="http://cran.r-project.org/src/contrib/Archive/argparser/argparser_0.1.tar.gz"
-if (!("argparser" %in% rownames(installed.packages))) {
-  install.packages(argparserurl, repos=NULL, type="source")
+optparseurl="http://cran.r-project.org/src/contrib/optparse_1.3.0.tar.gz"
+if (!("optparse" %in% rownames(installed.packages))) {
+  install.packages(optparseurl, repos=NULL, type="source")
 }
+
 dependencies = c("naturalsort")
 if (!all(dependencies %in% rownames(installed.packages))) {
   install.packages(setdiff(dependencies, rownames(installed.packages())), repos="http://cran.cnr.Berkeley.edu")
