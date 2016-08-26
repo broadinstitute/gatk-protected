f97fbbd361a983f9dea206a54d5b9bcd91d1d947
Samuel Lee <slee@broadinstitute.org>
2016-08-26 14:20:32 -0400

    updating test files, fixing more bugs

--- src/test/java/org/broadinstitute/hellbender/utils/plotter/ACNVPlotterUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/utils/plotter/ACNVPlotterUnitTest.java
@@ -13,7 +13,7 @@ public class ACNVPlotterUnitTest extends BaseTest {
     private static final String TOOLS_TEST_DIRECTORY = publicTestDir + "org/broadinstitute/hellbender/tools/exome/";
     private static final File TANGENT_NORMALIZED_COVERAGE_FILE = new File(TOOLS_TEST_DIRECTORY, "coverages-for-allelic-integration.tsv");
     private static final File TUMOR_ALLELIC_COUNTS_FILE = new File(TOOLS_TEST_DIRECTORY, "snps-for-allelic-integration.tsv");
-    private static final File SEGMENTS_FILE = new File("src/test/resources/org/broadinstitute/hellbender/utils/plotter/ACNV_final_segments.seg");
+    private static final File SEGMENTS_FILE = new File(TOOLS_TEST_DIRECTORY, "acnv-segments-from-allelic-integration.seg");
     private static final String SAMPLE_NAME = "test"; // This is what is in the segments file
     private static final String OUTPUT_PREFIX = "sample";
 
