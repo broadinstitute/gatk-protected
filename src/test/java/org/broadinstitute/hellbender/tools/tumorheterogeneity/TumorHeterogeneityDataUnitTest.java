6720900ffa22b5b7f82528d489c9c43e3d9cc5df
Samuel Lee <slee@broadinstitute.org>
2016-08-29 10:49:53 -0400

    THData

---
+++ src/test/java/org/broadinstitute/hellbender/tools/tumorheterogeneity/TumorHeterogeneityDataUnitTest.java
@@ -0,0 +1,12 @@
+package org.broadinstitute.hellbender.tools.tumorheterogeneity;
+
+import static org.testng.Assert.*;
+
+/**
+ * Unit tests for {@link TumorHeterogeneityData}.
+ *
+ * @author Samuel Lee &lt;slee@broadinstitute.org&gt;
+ */
+public class TumorHeterogeneityDataUnitTest {
+
+}
