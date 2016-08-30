8540e51459cdc0500847e02bc06312d0edec82c8
David Benjamin <davidben@broadinstitute.org>
2016-08-29 23:01:54 -0400

    polished PerAlleleCollectionUnitTest

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/mutect/PerAlleleCollectionUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/mutect/PerAlleleCollectionUnitTest.java
@@ -1,54 +1,46 @@
 package org.broadinstitute.hellbender.tools.walkers.mutect;
 
 import htsjdk.variant.variantcontext.Allele;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by tsato on 6/21/16.
  */
 public class PerAlleleCollectionUnitTest {
+    private static final Allele refA = Allele.create("A", true);
+    private static final Allele altA = Allele.create("A", false);
+    private static final Allele altC = Allele.create("C", false);
+    private static final Allele altG = Allele.create("G", false);
+    private static final Allele altT = Allele.create("T", false);
 
     @Test
     public void testSet() throws Exception {
         PerAlleleCollection<Integer> alleleCounts = new PerAlleleCollection<>(PerAlleleCollection.Type.REF_AND_ALT);
-        Allele refA = Allele.create("A", true);
-        Allele altT = Allele.create("T", false);
         alleleCounts.set(refA, 40);
         alleleCounts.set(altT, 10);
-        assertEquals((int)alleleCounts.getRef(), 40);
-        assertEquals((int)alleleCounts.getAlt(altT), 10);
+        Assert.assertEquals(alleleCounts.getRef().intValue(), 40);
+        Assert.assertEquals(alleleCounts.getAlt(altT).intValue(), 10);
     }
 
     @Test
     public void testGet() throws Exception {
         PerAlleleCollection<Integer> alleleCounts = new PerAlleleCollection<>(PerAlleleCollection.Type.REF_AND_ALT);
-        Allele refA = Allele.create("A", true);
-        Allele altT = Allele.create("T", false);
         alleleCounts.set(refA, 40);
         alleleCounts.set(altT, 10);
-        assertEquals((int)alleleCounts.get(refA), 40);
-        assertEquals((int)alleleCounts.get(altT), 10);
+        Assert.assertEquals(alleleCounts.get(refA).intValue(), 40);
+        Assert.assertEquals(alleleCounts.get(altT).intValue(), 10);
     }
-
 
     @Test
     public void testGetAltAlleles() throws Exception {
         PerAlleleCollection<Integer> alleleCounts = new PerAlleleCollection<>(PerAlleleCollection.Type.ALT_ONLY);
-        Allele altA = Allele.create("A", false);
-        Allele altC = Allele.create("C", false);
-        Allele altG = Allele.create("G", false);
-        Allele altT = Allele.create("T", false);
-        Allele[] altAlleles = {altA, altC, altG, altT};
-        for (Allele altAllele : altAlleles ) {
-            alleleCounts.set(altAllele, 3);
-        }
-
-        for (Allele altAllele : altAlleles ) {
-            assertTrue(alleleCounts.getAltAlleles().contains(altAllele));
-        }
-
-        assertFalse(alleleCounts.getAltAlleles().contains(Allele.create("A", true)));
+        final List<Allele> altAlleles = Arrays.asList(altA, altC, altG, altT);
+        altAlleles.forEach(a -> alleleCounts.set(a,3));
+        Assert.assertTrue(alleleCounts.getAltAlleles().containsAll(altAlleles));
+        Assert.assertFalse(alleleCounts.getAltAlleles().contains(refA));
     }
 }
