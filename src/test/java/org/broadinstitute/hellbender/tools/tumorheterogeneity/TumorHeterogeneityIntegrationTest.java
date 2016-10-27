ca69e11d78f392b8aba342d365722ba07572dd9f
Samuel Lee <lee.samuel.k@gmail.com>
2016-10-27 08:23:33 -0400

    decreasing length

--- src/test/java/org/broadinstitute/hellbender/tools/tumorheterogeneity/TumorHeterogeneityIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/tumorheterogeneity/TumorHeterogeneityIntegrationTest.java
@@ -14,8 +14,8 @@ import static org.testng.Assert.*;
  * @author Samuel Lee &lt;slee@broadinstitute.org&gt;
  */
 public final class TumorHeterogeneityIntegrationTest extends CommandLineProgramTest {
-    private static final File ACNV_SEGMENT_FILE = new File("/home/slee/working/ipython/purity-ploidy/integration-test/purity-0.8/total_segments-log2cr_sd-0.05-maf_sd-0.005.acnv.seg");
-//    private static final File ACNV_SEGMENT_FILE = new File("/home/slee/working/ipython/purity-ploidy/integration-test/purity-0.8/total_segments-log2cr_sd-0.001-maf_sd-0.001.acnv.seg");
+//    private static final File ACNV_SEGMENT_FILE = new File("/home/slee/working/ipython/purity-ploidy/integration-test/purity-0.8/total_segments-log2cr_sd-0.05-maf_sd-0.005.acnv.seg");
+    private static final File ACNV_SEGMENT_FILE = new File("/home/slee/working/ipython/purity-ploidy/integration-test/purity-0.8/total_segments-log2cr_sd-0.001-maf_sd-0.001.acnv.seg");
     private static final String OUTPUT_PREFIX = ACNV_SEGMENT_FILE.getAbsolutePath().replace(".acnv.seg", "");
 
     @Test
@@ -25,8 +25,8 @@ public final class TumorHeterogeneityIntegrationTest extends CommandLineProgramT
                 "--" + TumorHeterogeneity.OUTPUT_PREFIX_LONG_NAME, OUTPUT_PREFIX,
                 "--" + TumorHeterogeneity.NUM_SAMPLES_CLONAL_LONG_NAME, "200",
                 "--" + TumorHeterogeneity.NUM_BURN_IN_CLONAL_LONG_NAME, "150",
-                "--" + TumorHeterogeneity.NUM_SAMPLES_LONG_NAME, "500",
-                "--" + TumorHeterogeneity.NUM_BURN_IN_LONG_NAME, "400",
+                "--" + TumorHeterogeneity.NUM_SAMPLES_LONG_NAME, "200",
+                "--" + TumorHeterogeneity.NUM_BURN_IN_LONG_NAME, "150",
                 "--" + TumorHeterogeneity.NUM_CELLS_LONG_NAME, "50",
                 "--" + TumorHeterogeneity.METROPOLIS_ITERATION_FRACTION_CLONAL_LONG_NAME, "0.75",
                 "--" + TumorHeterogeneity.METROPOLIS_ITERATION_FRACTION_LONG_NAME, "0.25",
