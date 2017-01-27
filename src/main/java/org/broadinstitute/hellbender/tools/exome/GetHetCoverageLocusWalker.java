725a9240508bc9416411760c5ec06c34298e41df
lichtens <lichtens@broadinstitute.org>
2017-01-27 15:33:06 -0500

    Adding tumor sample processing

--- src/main/java/org/broadinstitute/hellbender/tools/exome/GetHetCoverageLocusWalker.java
+++ src/main/java/org/broadinstitute/hellbender/tools/exome/GetHetCoverageLocusWalker.java
@@ -5,10 +5,8 @@ import org.broadinstitute.barclay.argparser.Argument;
 import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
 import org.broadinstitute.hellbender.cmdline.ExomeStandardArgumentDefinitions;
 import org.broadinstitute.hellbender.cmdline.programgroups.CopyNumberProgramGroup;
-import org.broadinstitute.hellbender.engine.AlignmentContext;
-import org.broadinstitute.hellbender.engine.FeatureContext;
-import org.broadinstitute.hellbender.engine.LocusWalker;
-import org.broadinstitute.hellbender.engine.ReferenceContext;
+import org.broadinstitute.hellbender.engine.*;
+import org.broadinstitute.hellbender.exceptions.UserException;
 import org.broadinstitute.hellbender.tools.exome.alleliccount.AllelicCount;
 import org.broadinstitute.hellbender.tools.exome.alleliccount.AllelicCountCollection;
 import org.broadinstitute.hellbender.tools.exome.alleliccount.AllelicCountTableColumn;
@@ -16,14 +14,20 @@ import org.broadinstitute.hellbender.tools.exome.pulldown.HetPulldownCalculator;
 import org.broadinstitute.hellbender.utils.Nucleotide;
 import org.broadinstitute.hellbender.utils.read.GATKRead;
 import org.broadinstitute.hellbender.utils.read.ReadConstants;
+import org.broadinstitute.hellbender.utils.read.ReadUtils;
 
 import java.io.File;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 @CommandLineProgramProperties(
-        summary = "EXPERIMENTAL ... Output ref/alt counts at heterozygous SNPs in normal sample (and at same sites in tumor sample, if specified).",
+        summary = "EXPERIMENTAL ... Output ref/alt counts at heterozygous SNPs in normal sample (and at same sites in tumor sample, if specified).\n" +
+                "\t\tNOTE:  This method assumes that the first input bam file is the normal (control) and the second is the tumor (case)." +
+                "\n\t\tNOTE:  This method requires the SM tag from the normal bam file in order to work.  ",
         oneLineSummary = "Output ref/alt counts at heterozygous SNPs in normal sample (and at same sites in tumor sample, if specified)",
         programGroup = CopyNumberProgramGroup.class
 )
@@ -34,14 +38,6 @@ public final class GetHetCoverageLocusWalker extends LocusWalker {
 
     protected static final String MINIMUM_READ_COUNT_SHORT_NAME = "minRC";
     protected static final String MINIMUM_READ_COUNT_FULL_NAME = "minimumReadCount";
-
-    @Argument(
-            doc = "BAM file for tumor sample.",
-            fullName = ExomeStandardArgumentDefinitions.TUMOR_BAM_FILE_LONG_NAME,
-            shortName = ExomeStandardArgumentDefinitions.TUMOR_BAM_FILE_SHORT_NAME,
-            optional = true
-    )
-    protected File tumorBAMFile;
 
     @Argument(
             doc = "Output file for normal-sample ref/alt read counts (at heterozygous SNPs).",
@@ -97,45 +93,101 @@ public final class GetHetCoverageLocusWalker extends LocusWalker {
         return true;
     }
 
-    private AllelicCountCollection hetPulldown = new AllelicCountCollection();
+    private AllelicCountCollection controlHetPulldown = new AllelicCountCollection();
+
+    private Map<String, AllelicCountCollection> caseSampleNameToHetPulldowns = new HashMap<>();
+
+    private String controlSampleName = null;
+
 
     @Override
     public void onTraversalStart() {
-        logger.info("Starting...");
+        // HACK:  get the normal (first -I) file
+        final List<Path> readArguments = this.readArguments.getReadPaths();
+
+        if (readArguments.size() > 2) {
+            throw new UserException.BadInput("Please specify only two bam files.  The first is the normal and the second is the tumor.  Changes to this convention are coming soon.");
+        }
+
+        // Get the normal sample name
+        final ReadsDataSource normalReads = new ReadsDataSource(readArguments.get(0));
+        controlSampleName = ReadUtils.getSamplesFromHeader(normalReads.getHeader()).iterator().next();
+
+        // Get the tumor sample name
+        final List<Path> casePaths = readArguments.subList(1, readArguments.size());
+        for (final Path casePath : casePaths) {
+            final ReadsDataSource caseReads = new ReadsDataSource(casePath);
+            final String sampleName = ReadUtils.getSamplesFromHeader(caseReads.getHeader()).iterator().next();
+
+            caseSampleNameToHetPulldowns.put(sampleName, new AllelicCountCollection());
+        }
+
+        logger.info("Starting... N: " + controlSampleName + "   T: " + String.join(", ", caseSampleNameToHetPulldowns.keySet()));
+
     }
 
     @Override
     public void apply(AlignmentContext alignmentContext, ReferenceContext referenceContext, FeatureContext featureContext) {
 
+        final Map<String, AlignmentContext> sampleContexts = alignmentContext.splitContextBySampleName(getHeaderForReads());
+
+        // TODO: Check that no two samples have the same name.
+        // TODO: edge case where my normal is an empty bam
         // TODO: Error checking
         // TODO: Do I need to filter the reads?
-        final List<GATKRead> allReads = alignmentContext.getBasePileup().getReads();
 
-        if (allReads.size() < minimumRawReads) {
+        // Get the reads from the normal
+        final AlignmentContext alignmentContextForNormal = sampleContexts.get(controlSampleName);
+        if (alignmentContextForNormal == null) {
+            return;
+        }
+        final List<GATKRead> allNormalReads = alignmentContextForNormal.getBasePileup().getReads();
+
+        if (allNormalReads.size() < minimumRawReads) {
             return;
         }
         final byte refAsByte = referenceContext.getBase();
 
-        final List<Integer> allOffsets = alignmentContext.getBasePileup().getOffsets();
+        final List<Integer> allNormalOffsets = alignmentContextForNormal.getBasePileup().getOffsets();
 
         final Nucleotide.Counter baseCounts = new Nucleotide.Counter();
-        IntStream.range(0, allReads.size()).forEach(i-> baseCounts.add(allReads.get(i).getBase(allOffsets.get(i))));
-        final int totalBaseCount = Arrays.stream(HetPulldownCalculator.BASES).mapToInt(b -> (int) baseCounts.get(b)).sum();
+        IntStream.range(0, allNormalReads.size()).forEach(i -> baseCounts.add(allNormalReads.get(i).getBase(allNormalOffsets.get(i))));
+        final int totalBaseCountInNormal = Arrays.stream(HetPulldownCalculator.BASES).mapToInt(b -> (int) baseCounts.get(b)).sum();
 
-        // TODO: Make sure we are working with the normal
-        if (!HetPulldownCalculator.isPileupHetCompatible(baseCounts, totalBaseCount, pvalThreshold)) {
-            return;
-        } else {
+        if (HetPulldownCalculator.isPileupHetCompatible(baseCounts, totalBaseCountInNormal, pvalThreshold)) {
 
-            final int refReadCount = (int) baseCounts.get(Nucleotide.valueOf(refAsByte));
-            final int altReadCount = totalBaseCount - refReadCount;
-            hetPulldown.add(new AllelicCount(referenceContext.getInterval(), refReadCount, altReadCount));
+            final int refReadCountInNormal = (int) baseCounts.get(Nucleotide.valueOf(refAsByte));
+            final int altReadCountInNormal = totalBaseCountInNormal - refReadCountInNormal;
+            controlHetPulldown.add(new AllelicCount(referenceContext.getInterval(), refReadCountInNormal, altReadCountInNormal));
+
+            // Process case samples
+            for (final String caseSampleName : caseSampleNameToHetPulldowns.keySet()) {
+                final AlignmentContext alignmentContextForCase = sampleContexts.get(caseSampleName);
+                if (alignmentContextForCase == null) {
+                    return;
+                }
+                final List<GATKRead> allCaseReads = alignmentContextForCase.getBasePileup().getReads();
+                if (allCaseReads.size() < minimumRawReads) {
+                    continue;
+                }
+                final List<Integer> allCaseOffsets = alignmentContextForCase.getBasePileup().getOffsets();
+                final Nucleotide.Counter caseBaseCounts = new Nucleotide.Counter();
+                IntStream.range(0, allCaseReads.size()).forEach(i -> caseBaseCounts.add(allCaseReads.get(i).getBase(allCaseOffsets.get(i))));
+                final int totalBaseCountInCase = Arrays.stream(HetPulldownCalculator.BASES).mapToInt(b -> (int) caseBaseCounts.get(b)).sum();
+                final int refReadCountInCase = (int) baseCounts.get(Nucleotide.valueOf(refAsByte));
+                final int altReadCountInCase = totalBaseCountInCase - refReadCountInCase;
+                caseSampleNameToHetPulldowns.get(caseSampleName).add(new AllelicCount(referenceContext.getInterval(), refReadCountInCase, altReadCountInCase));
+            }
         }
     }
 
     @Override
     public Object onTraversalSuccess() {
-        hetPulldown.write(normalHetOutputFile, AllelicCountTableColumn.AllelicCountTableVerbosity.BASIC);
+        controlHetPulldown.write(normalHetOutputFile, AllelicCountTableColumn.AllelicCountTableVerbosity.BASIC);
+        // TODO: Update to allow more than one case sample.  This next line assumes one case sample, since it writes to the same output file no matter what.
+        for (final Map.Entry<String, AllelicCountCollection> caseEntry : caseSampleNameToHetPulldowns.entrySet()) {
+            caseEntry.getValue().write(tumorHetOutputFile, AllelicCountTableColumn.AllelicCountTableVerbosity.BASIC);
+        }
         return "SUCCESS";
     }
 }
