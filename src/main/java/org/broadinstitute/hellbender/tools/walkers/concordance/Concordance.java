8bf09a2cf15531b214d3e2c1a3600f3ee4cedc58
Takuto Sato <tsato@broadinstitute.org>
2017-02-23 14:23:23 -0500

    use VariantContextComparator

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/concordance/Concordance.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/concordance/Concordance.java
@@ -1,7 +1,9 @@
 package org.broadinstitute.hellbender.tools.walkers.concordance;
 
 
+import htsjdk.samtools.SAMSequenceDictionary;
 import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.variantcontext.VariantContextComparator;
 import org.broadinstitute.barclay.argparser.Argument;
 import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
 import org.broadinstitute.hellbender.cmdline.programgroups.VariantProgramGroup;
@@ -20,6 +22,15 @@ import java.util.Iterator;
 )
 
 /**
+ *
+ * This tool compares an evaluation vcf against a truth vcf. We assume that the truth vcf only contains PASS variants.
+ * The summary statistics (# true positives, # false positives, # false negatives, sensitivity, precision)
+ * are reported in the summary .tsv (--summary). Also reported as a .tsv (--table) is the basic information of
+ * interesting variants i.e. true positives, false positives, false negatives for further statistical analysis
+ * e.g. to be read in by an R script
+ *
+ * java -jar gatk.jar Concordance -V TODO fill in
+ *
  * Created by tsato on 1/30/17.
  */
 // TODO: maybe this tool should be a variant walker. Or a multiple variant walker.
@@ -59,6 +70,7 @@ public class Concordance extends VariantWalker {
     private VariantContext currentTruthVariant;
 
     private boolean exhaustedTruthVariants = false;
+    private VariantContextComparator variantContextComparator;
 
     @Override
     public void onTraversalStart() {
@@ -71,10 +83,12 @@ public class Concordance extends VariantWalker {
         variantStatusWriter = VariantStatusRecord.getWriter(table);
         concordanceSummaryWriter = ConcordanceSummaryRecord.getWriter(summary);
 
+        SAMSequenceDictionary dictionary = getSequenceDictionaryForDrivingVariants();
+        variantContextComparator = new VariantContextComparator(dictionary);
 
         if (! getHeaderForVariants().getSampleNamesInOrder().contains(tumorSampleName)){
             // TODO: do I need to close files?
-            throw new IllegalArgumentException(String.format("the tumor sample %s does not exit in vcf", tumorSampleName));
+            throw new IllegalArgumentException(String.format("the tumor sample %s does not exist in vcf", tumorSampleName));
         }
 
         final FeatureDataSource<VariantContext> truthSource = new FeatureDataSource<>(truth);
@@ -84,6 +98,9 @@ public class Concordance extends VariantWalker {
 
     @Override
     public void apply(VariantContext variant, ReadsContext readsContext, ReferenceContext referenceContext, FeatureContext featureContext ) {
+        // TODO: when we give the tool an interval List (-L), variant walker limits the range of driving variants to these intervals
+        // but it doesn't do the same for the truth vcf i.e. truth vcf starts starts at chr 1 when -L 21.
+
         // no more truth variants; eval variant is a FP unless filtered
         if (exhaustedTruthVariants){
             if (variant.isNotFiltered()){
@@ -98,7 +115,7 @@ public class Concordance extends VariantWalker {
         // move the truth cursor forward until it catches up to eval
         // note we must check for this first, *then* check for the other two conditions
         // otherwise we move the eval forward a without giving it a diagnosis
-        while (comparePositions(variant, currentTruthVariant) > 0) {
+        while (variantContextComparator.compare(variant, currentTruthVariant) > 0) {
             falseNegatives++;
             if (truthIterator.hasNext()) {
                 currentTruthVariant = truthIterator.next();
@@ -111,7 +128,7 @@ public class Concordance extends VariantWalker {
         // TODO: use a comparator?
         // TODO: need more asserts to ensure that we aren't breaking invariants
         // case 2: the position of the two variants match
-        if (comparePositions(variant, currentTruthVariant) == 0) {
+        if (variantContextComparator.compare(variant, currentTruthVariant) == 0) {
             // do the genotypes match too?
             if (variant.isFiltered()) {
                 falseNegatives++;
@@ -139,7 +156,7 @@ public class Concordance extends VariantWalker {
         }
 
         // case 3: truth got ahead of eval; the eval variant must be a false positive if not filtered
-        if (comparePositions(variant, currentTruthVariant) < 0) {
+        if (variantContextComparator.compare(variant, currentTruthVariant) < 0) {
             if (variant.isNotFiltered()) {
                 falsePositives++;
                 writeThisRecord(new VariantStatusRecord(variant, FALSE_POSITIVE, tumorSampleName), variant);
@@ -190,25 +207,8 @@ public class Concordance extends VariantWalker {
     private static String FALSE_POSITIVE_AND_FALSE_NEGATIVE = "FALSE_POSITIVE_AND_FALSE_NEGATIVE";
     private static String FALSE_POSITIVE = "FALSE_POSITIVE";
 
-    // TODO: make variantcontext comparable?
-    protected static int comparePositions(final VariantContext truth, final VariantContext eval){
-        if (! truth.getContig().equals(eval.getContig()) ) {
-            // TODO: make sure x, y, and mt work as expected
-            return truth.getContig().compareTo(eval.getContig());
-        } else {
-            // same chromosome, compare the start positions
-            if (truth.getStart() > eval.getStart()) {
-                return 1;
-            } else if (truth.getStart() < eval.getStart()){
-                return -1;
-            } else {
-                return 0;
-            }
-        }
-    }
-
     // TODO: eventually this should be an abstract method to be overridden by a subclass
-    protected static boolean allelesMatch(final VariantContext truth, final VariantContext eval){
+    protected static boolean allelesMatch(final VariantContext eval, final VariantContext truth){
         final boolean sameContig = truth.getContig().equals(eval.getContig());
         final boolean sameStartPosition = truth.getStart() == eval.getStart();
         final boolean sameRefAllele = truth.getReference().equals(eval.getReference());
