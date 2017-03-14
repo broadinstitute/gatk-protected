45ac5e25b1bc9cb29c7100264c71d54ec88e4860
lichtens <lichtens@broadinstitute.org>
2017-03-14 11:24:06 -0400

    Making explicit json path.

--- scripts/m2_cromwell_tests/run_m2_wdl.sh
+++ scripts/m2_cromwell_tests/run_m2_wdl.sh
@@ -5,4 +5,4 @@ script_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 cd "$script_path"
 
 
-java -jar ~/cromwell-0.25.jar run /home/travis/build/broadinstitute/gatk-protected/scripts/mutect2_wdl/mutect2_multi_sample.wdl test_m2_wdl_multi_mod.json
+java -jar ~/cromwell-0.25.jar run /home/travis/build/broadinstitute/gatk-protected/scripts/mutect2_wdl/mutect2_multi_sample.wdl /home/travis/build/broadinstitute/test_m2_wdl_multi_mod.json
