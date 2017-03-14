d5c5c80e50e940c56db9444e9e1553776c83d361
lichtens <lichtens@broadinstitute.org>
2017-03-14 14:32:48 -0400

    Trying to diagnose cromwell issue.

--- scripts/m2_cromwell_tests/run_m2_wdl.sh
+++ scripts/m2_cromwell_tests/run_m2_wdl.sh
@@ -5,4 +5,4 @@ script_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 cd "$script_path"
 
 ln -s /home/travis/build/broadinstitute/gatk-protected/scripts/mutect2_wdl/mutect2.wdl
-java -jar ~/cromwell-0.25.jar run /home/travis/build/broadinstitute/gatk-protected/scripts/mutect2_wdl/mutect2_multi_sample.wdl /home/travis/build/broadinstitute/test_m2_wdl_multi_mod.json
+java -jar ~/cromwell-0.25.jar run /home/travis/build/broadinstitute/gatk-protected/scripts/mutect2_wdl/mutect2_multi_sample.wdl /home/travis/build/broadinstitute/test_m2_wdl_multi_mod.json - test_m2_wdl.metadata
